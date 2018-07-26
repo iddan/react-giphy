8e3d8c60555f90d2bd0e8fe1fbd68fbf50323802
Iddan Aaronsohn <mail@aniddan.com>
2018-07-27 01:57:20 +0300

    Updated flow, exposing types Pintereset style

--- src/types.js
+++ src/types.js
@@ -1,66 +1,66 @@
 // @flow
 
-type BaseImage = {
-  url: string;
-  width: string;
-  height: string;
-};
+type BaseImage = {|
+  url: string,
+  width: string,
+  height: string
+|};
 
-type SizedImage = {
+type SizedImage = {|
   ...BaseImage,
-  size: string;
-}
+  size: string
+|};
 
-export type Images = {
-  fixed_height: {
+export type Images = {|
+  fixed_height: {|
     ...SizedImage,
     mp4: string,
     mp4_size: string,
     webp: string,
     webp_size: string
-  },
+  |},
   fixed_height_still: BaseImage,
-  fixed_height_downsampled: {
+  fixed_height_downsampled: {|
     ...SizedImage,
     webp: string,
     webp_size: string
-  },
-  fixed_width: {
+  |},
+  fixed_width: {|
     ...SizedImage,
     mp4: string,
     mp4_size: string,
     webp: string,
     webp_size: string
-  },
+  |},
   fixed_width_still: BaseImage,
-  fixed_width_downsampled: {
+  fixed_width_downsampled: {|
     ...SizedImage,
     webp: string,
     webp_size: string
-  },
-  fixed_height_small: {
+  |},
+  fixed_height_small: {|
     ...SizedImage,
     mp4: string,
     mp4_size: string,
     webp: string,
     webp_size: string
-  },
+  |},
   fixed_height_small_still: BaseImage,
-  fixed_width_small: {
+  fixed_width_small: {|
     ...SizedImage,
     mp4: string,
     mp4_size: string,
     webp: string,
     webp_size: string
-  },
-  fixed_width_small_still: {
+  |},
+  fixed_width_small_still: {|
     ...SizedImage,
-    downsized: string,
-  },
+    downsized: string
+  |},
   downsized_still: BaseImage,
   downsized_large: SizedImage,
   downsized_medium: SizedImage,
-  downsized_small: {
+  downsized_small: {|
     ...SizedImage,
     original: string,
     frames: string,
@@ -68,26 +68,26 @@ export type Images = {
     mp4_size: string,
     webp: string,
     webp_size: string
-  }
-};
+  |}
+|};
 
-export type GIFObject = {
-  type: string;
-  id: string;
-  slug: string;
-  url: string;
-  bitly_url: string;
-  embed_url: string;
-  username: string;
-  source: string;
-  rating: string;
-  user: string;
-  source_tld: string;
-  source_post_url: string;
-  update_datetime: string;
-  create_datetime: string;
-  import_datetime: string;
-  trending_datetime: string;
-  images: Images;
-  title: string;
-}
+export type GIFObject = {|
+  type: string,
+  id: string,
+  slug: string,
+  url: string,
+  bitly_url: string,
+  embed_url: string,
+  username: string,
+  source: string,
+  rating: string,
+  user: string,
+  source_tld: string,
+  source_post_url: string,
+  update_datetime: string,
+  create_datetime: string,
+  import_datetime: string,
+  trending_datetime: string,
+  images: Images,
+  title: string
+|};
